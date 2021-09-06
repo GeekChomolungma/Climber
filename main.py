5c2773e0a8a8b8b30e96e6a5d61c14a6ea25f926
HarvestStar <574817827@qq.com>
2021-09-06 22:05:05 +0800

    Run V3

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ conn_str = "mongodb://market:admin123@139.196.155.97:27017"
 cm = cm.CmMacd(conn_str)
 cm.LoadDB("marketinfo")
 symbols = ["btcusdt","ethusdt","bchusdt","ltcusdt"]
-cm.RunV2("1min",300,symbols,"30min")
+#cm.RunV2("1min",300,symbols,"30min")
+cm.RunV3(symbols, 300, "30min", "4hour")
